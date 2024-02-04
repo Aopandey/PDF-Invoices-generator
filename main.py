5c488d125404fb5e8998a644f50c2c9190cec1b9
Avinash <avkr258@gmail.com>
2024-02-04 15:36:36 -0500

    Created PDF for each Excel files

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import pandas as zx
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepath = glob.glob("invoices/*.xlsx")
 
 for i in filepath:
     data = zx.read_excel(i, sheet_name="Sheet 1")
-    print(data)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.add_page()
+    filename = Path(i).stem
+    invoice_num = filename.split("-")[0]
+    pdf.cell(w=50, h=8, txt=f"Invoice no. {invoice_num}")
+    pdf.output(f"PDF/{filename}.pdf")
+
+
