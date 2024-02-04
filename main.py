6c19cbc245a90799430ead35b2fec077b06251f1
Avinash <avkr258@gmail.com>
2024-02-04 17:16:16 -0500

    Added date

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ filepath = glob.glob("invoices/*.xlsx")
 
 for i in filepath:
     data = zx.read_excel(i, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.set_font(family="Times", size=16, style="B")
     pdf.add_page()
+
     filename = Path(i).stem
     invoice_num = filename.split("-")[0]
-    pdf.cell(w=50, h=8, txt=f"Invoice no. {invoice_num}")
+    date = filename.split("-")[1]
+
+    pdf.cell(w=50, h=8, txt=f"Invoice no. {invoice_num}", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
     pdf.output(f"PDF/{filename}.pdf")
 
 
